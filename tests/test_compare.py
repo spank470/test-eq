5f5d8bf5d0c9484d67f34bf01369049ea07db660
Spank8 <spank8@vk.com>
2025-04-16 11:07:30 +0300

    Tests9

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -41,3 +41,23 @@ class TestCompareHTML:
 
         with allure.step("Сравнение HTML содержимого"):
             assert ref.text == target.text, "HTML ответы отличаются!"
+            
+    
+    @allure.title("3 - REF и TARGET различаются")
+    @allure.description("Проверяем, что страницы REF и TARGET возвращают одинаковый HTML и статус код 200.")
+    def test_compare_html_responses_error(self):
+        url_ref = "http://ya.ru"
+        url_target = "http://google.com"
+
+        with allure.step(f"GET запрос к REF: {url_ref}"):
+            ref = requests.get(url_ref)
+            allure.attach(ref.text, name="REF Response", attachment_type=allure.attachment_type.TEXT)
+            assert ref.status_code == 200
+
+        with allure.step(f"GET запрос к TARGET: {url_target}"):
+            target = requests.get(url_target)
+            allure.attach(target.text, name="TARGET Response", attachment_type=allure.attachment_type.TEXT)
+            assert target.status_code == 200
+
+        with allure.step("Сравнение HTML содержимого"):
+            assert ref.text == target.text, "HTML ответы отличаются!"
